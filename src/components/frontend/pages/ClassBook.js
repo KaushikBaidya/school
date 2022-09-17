53f470847e6d707deb7f3fcb7b12f5163a8033a8
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2022-09-17 14:50:52 +0600

    Create ClassBook.js

---
+++ src/components/frontend/pages/ClassBook.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ClassBook = () => {
+  return (
+    <div className="max-w-6xl px-10 lg:mx-auto py-28 ">
+      <h1 className="text-center text-4xl text-[#20702E] font-semibold uppercase">
+        Class Book List
+      </h1>
+    </div>
+  );
+};
+
+export default ClassBook;
