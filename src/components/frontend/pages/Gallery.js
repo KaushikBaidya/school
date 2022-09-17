f5835995b2c724eb673907dc4db623eff34d28fc
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2022-09-17 17:39:19 +0600

    Create Gallery.js

---
+++ src/components/frontend/pages/Gallery.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Gallery = () => {
+  return (
+    <div className="max-w-6xl px-10 lg:mx-auto py-28 ">
+      <h1 className="text-center text-4xl text-[#20702E] font-semibold uppercase">
+        Photo Gallery
+      </h1>
+    </div>
+  );
+};
+
+export default Gallery;
