9050cab9c72f88602f424d88834788d2900f5111
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2022-09-20 14:04:06 +0600

    Update Job.js

--- src/components/frontend/pages/Job.js
+++ src/components/frontend/pages/Job.js
@@ -2,90 +2,24 @@ import React from "react";
 
 const Job = () => {
   return (
-    <div className="max-w-6xl px-10 lg:mx-auto py-28 ">
-      <h1 className="text-center text-4xl text-[#20702E] font-semibold uppercase">
-        Job Vacancy
+    <div className="max-w-6xl flex flex-col items-center gap-4 px-10 lg:mx-auto py-28 ">
+      <h1 className="text-center text-xl lg:text-4xl text-[#20702E] font-semibold uppercase">
+        About Us
       </h1>
-      {/* <div className="flex flex-col">
-        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-4 inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="overflow-hidden">
-              <table className="min-w-full text-center">
-                <thead className="border-b bg-gray-800">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-white px-6 py-4"
-                    >
-                      SL
-                    </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-white px-6 py-4"
-                    >
-                      Details
-                    </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-white px-6 py-4"
-                    >
-                      Passed Students
-                    </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-white px-6 py-4"
-                    >
-                      Percentage
-                    </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-white px-6 py-4"
-                    >
-                      GPA 5
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className="bg-white border-b">
-                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                      2021
-                    </td>
-                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      95
-                    </td>
-                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      85
-                    </td>
-                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      90%
-                    </td>
-                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      35
-                    </td>
-                  </tr>
-                  <tr className="bg-white border-b">
-                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                      2022
-                    </td>
-                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      80
-                    </td>
-                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      80
-                    </td>
-                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      100%
-                    </td>
-                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      48
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div> */}
+      <hr className="w-24 h-1 bg-[#EC1B23]" />
+      <div className="max-w-4xl mx-auto p-5 text-left border-2 border-slate-800 rounded-lg my-5">
+        <h3 className="text-xl uppercase text-gray-700">Job Title</h3>
+        <p className="text-base text-gray-600">English Teacher</p>
+        <h3 className="text-xl uppercase text-gray-700">Vacancy</h3>
+        <p className="text-base uppercase text-gray-600">2</p>
+        <h3 className="text-xl uppercase text-gray-700">Qualifications</h3>
+        <p className="text-base text-gray-600 text-justify">
+          At least Bachelor's degree Previous experience. The ability to
+          differentiate and deliver conceptual, engaging and inquiry-based
+          lessons. Familiarity and experience with collaborative planning and a
+          team-teaching approach.
+        </p>
+      </div>
     </div>
   );
 };
