9e0a5e15adf5cb5d50b243fea458a59db101bc77
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2022-09-20 14:05:54 +0600

    f

--- src/components/frontend/pages/Gallery.js
+++ src/components/frontend/pages/Gallery.js
@@ -2,10 +2,11 @@ import React from "react";
 
 const Gallery = () => {
   return (
-    <section className="max-w-7xl px-10 lg:mx-auto py-28 ">
-      <h1 className="text-center text-4xl text-[#20702E] font-semibold uppercase">
+    <section className="max-w-6xl flex flex-col items-center gap-4 px-10 lg:mx-auto py-28 ">
+      <h1 className="text-center text-xl lg:text-4xl text-[#20702E] font-semibold uppercase">
         Photo Gallery
       </h1>
+      <hr className="w-24 h-1 bg-[#EC1B23]" />
       <div className="overflow-hidden">
         <div className="px-5 py-2 mx-auto lg:pt-12 lg:px-32">
           <div className="flex flex-wrap -m-1 md:-m-2">
--- src/components/frontend/pages/Job.js
+++ src/components/frontend/pages/Job.js
@@ -4,7 +4,7 @@ const Job = () => {
   return (
     <div className="max-w-6xl flex flex-col items-center gap-4 px-10 lg:mx-auto py-28 ">
       <h1 className="text-center text-xl lg:text-4xl text-[#20702E] font-semibold uppercase">
-        About Us
+        Job Vacancy
       </h1>
       <hr className="w-24 h-1 bg-[#EC1B23]" />
       <div className="max-w-4xl mx-auto p-5 text-left border-2 border-slate-800 rounded-lg my-5">
